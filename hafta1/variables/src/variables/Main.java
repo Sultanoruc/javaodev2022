b01e44b340f6eb9c626296e2007fd304aaa2e637
Sultanoruc <114101754+Sultanoruc@users.noreply.github.com>
2022-09-25 21:20:17 +0300

    hafta1ödev

---
+++ hafta1/variables/src/variables/Main.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+package variables;
+
+/**
+ * @author SULTAN ORUÇ
+ *
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		int ogrenciSayisi = 12;
+		String mesaj ="Öğrenci sayısı: ";
+		System.out.println(mesaj + ogrenciSayisi);
+		System.out.println(mesaj + ogrenciSayisi);
+		System.out.println("Öğrenci sayım : "+ogrenciSayisi);
+		System.out.println("Öğrenci sayım : "+ogrenciSayisi);
+		System.out.println("Öğrenci sayım : "+ogrenciSayisi);
+	}
+
+}
